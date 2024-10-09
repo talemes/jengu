ec284cdb0a588d18d88b3d2f3ee824249e7c7d4e
gustavofaria-jpg <gustavo.fj2006@aluno.ifsc.edu.br>
2024-10-09 20:44:35 +0000

    movimentação de personagem

--- config.js
+++ config.js
@@ -1,6 +1,20 @@
 export default {
     type: Phaser.AUTO,
-    width: 480,
-    height: 800,
-    parent: 'game-container'
+    width: 800,
+    height: 450,
+    parent: 'game-container',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: true
+        }
+    },
+    scale: {
+        mode: Phaser.Scale.FIT,
+        parent: 'game-container',
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 800,
+        height: 450
+    }
 }
--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import config from './config.js'
+import abertura from './abertura.js'
+import sala from './sala.js'
 
-class Game extends Phaser.Game 
-{
-    constructor (){
-        super(config) 
- }
+class Game extends Phaser.Game {
+    constructor () {
+        super(config)
+
+        this.scene.add('abertura', abertura)
+        this.scene.add('sala', sala)
+        this.scene.start('abertura')
+    }
 }
 
-window.onload = () =>{
-window.game = new Game()
+window.onload = () => {
+    window.game = new Game()
 }
